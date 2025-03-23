8219c54d65cd5bfd554c00c9c3fe8c337c8f5579
irinesarin <irinesarin@gmail.com>
2025-03-23 15:31:29 +0530

    Simplify product page component

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -41,16 +41,7 @@ const product = {
   ],
 }
 
-type PageProps = {
-  params: Promise<{
-    id: string
-  }>
-  searchParams?: { [key: string]: string | string[] | undefined }
-}
-
-export default async function ProductPage(props: PageProps) {
-  const { id } = await props.params;
-
+export default function ProductPage({ params }: { params: { id: string } }) {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
